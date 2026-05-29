3707a04547e3514b95c9e13b74a4aada169a80c8
Tariq Odeh <tariq.odeh.dev@email.com>
2026-05-29 12:30:09 +0000

    Tariq

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,4 @@
 import logo from "../assets/Group.svg"
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LockIcon from '@mui/icons-material/Lock';
-import Button from '@mui/material/Button';
-
 function Login() {
     return (
         <div className="min-h-screen bg-gradient-to-br from-[#F5F1EB] to-[#DDD6CE] flex items-center justify-center">
@@ -11,17 +7,16 @@ function Login() {
 
                 {/* Top Section */}
                 <div className="bg-gradient-to-b from-[#8B1E1E] to-[#6E1414] h-64 flex flex-col items-center justify-center text-white">
-
                     <img
                         src={logo}
                         alt="logo"
                         className="w-24 mb-4"
-                    />
+                       />
                     <h1 className="font-['lemon'] text-4xl font-bold mb-2">
                         Welcome back sir
                     </h1>
 
-                    <p className="font-['lemon'] text-gray-200">
+                    <p className="text-gray-200">
                         Sign in to manage your restaurant
                     </p>
 
@@ -29,80 +24,71 @@ function Login() {
 
                 {/* Bottom Section */}
                 <div className="p-10 flex flex-col items-center">
+
                     <div className="mb-6">
 
-                        <label className="block text-xl font-['lemon'] mb-2 text-gray-700">
+                        <label className="block text-xl mb-2 text-gray-700">
                             USERNAME
                         </label>
 
-                        <div className="w-[400px]  border border-gray-400 rounded-xl p-4 flex items-center gap-3 focus-within:border-[#7F1D1D] transition duration-300">
+                        <div className="w-[400px] border border-gray-400 rounded-xl p-4 flex items-center gap-3">
 
-                            <AccountCircleIcon className="text-yellow-600" />
+                            <span>👤</span>
 
                             <input
                                 type="email"
                                 placeholder="Enter your email"
-                                className="outline-none w-full font-['lemon']"
+                                className="outline-none w-full"
                             />
 
                         </div>
 
                     </div>
+
                     <div className="mb-6">
 
-                        <label className="block  font-['lemon'] mb-2 text-gray-700 text-xl">
+                        <label className="block mb-2 text-gray-700 text-xl">
                             PASSWORD
                         </label>
 
-                        <div className="w-[400px] border border-gray-400 rounded-xl p-4 flex items-center gap-3 focus-within:border-[#7F1D1D] transition duration-300">
+                        <div className="w-[400px] border border-gray-400 rounded-xl p-4 flex items-center gap-3">
 
-                            <LockIcon className="text-yellow-600" />
+                            <span>🔒</span>
+
                             <input
                                 type="password"
                                 placeholder="Enter your password"
-                                className="outline-none w-full font-['lemon'] "
+                                className="outline-none w-full"
                             />
 
                         </div>
 
                     </div>
+
                     <div className="w-[400px] flex items-center justify-between mb-6">
 
                         <div className="flex items-center gap-2">
 
                             <input type="checkbox" />
 
-                            <p className="font-['lemon'] text-sm text-gray-600">
+                            <p className="text-sm text-gray-600">
                                 Remember me
                             </p>
 
                         </div>
 
-                        <button className="font-['lemon'] text-sm text-[#7F1D1D] font-semibold">
+                        <button className="text-sm text-[#7F1D1D] font-semibold">
                             Forgot password?
                         </button>
 
                     </div>
-                    <Button
-                        variant="contained"
-                        fullWidth
-                        sx={{
-                            backgroundColor: "#7F1D1D",
-                            padding: "14px",
-                            borderRadius: "14px",
-                            fontSize: "30px",
-                            fontFamily: "lemon",
-                            textTransform: "none",
-                            marginTop: "10px",
 
-                            "&:hover": {
-                                backgroundColor: "#6E1414",
-                            },
-                        }}
+                    <button
+                        className="w-full bg-[#7F1D1D] text-white p-4 rounded-xl text-3xl mt-3"
                     >
                         Sign in
-                    </Button>
-                   
+                    </button>
+
                 </div>
 
             </div>
