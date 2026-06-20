408445f260e34d97d64b83b23e4483c195b3179b
FayezAlhanash <fayez.alhanash2005@gmail.com>
2026-06-20 18:26:33 +0300

    EMPLOYEE MANAGEMNT

--- src/components/Admin/SideBar.jsx
+++ src/components/Admin/SideBar.jsx
@@ -1,3 +1,4 @@
+
 import {
     LayoutDashboard,
     UtensilsCrossed,
@@ -7,9 +8,10 @@ import {
     Settings,
     LogOut
 } from "lucide-react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function SideBar() {
+    const navigate = useNavigate();
     const menu = [
         {
             icon: <LayoutDashboard size={20} />,
@@ -37,7 +39,10 @@ function SideBar() {
             path: "/tables",
         },
     ];
-
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/", { replace: true });
+    };
     return (
         <div
             className="
@@ -90,7 +95,15 @@ top-0
                 ))}
 
             </div>
-
+            <div className="p-4 border-t">
+                <button
+                    onClick={handleLogout}
+                    className="w-full flex items-end gap-3 px-4 py-3 rounded-xl text-red-600 hover:bg-red-50 transition-all duration-300"
+                >
+                    <LogOut size={20} />
+                    <span>Logout</span>
+                </button>
+            </div>
         </div>
     );
 }
